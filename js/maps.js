8ced26a1e9f1da960bcf716df747cb539517b1a9
Michael Coombes <mc6415@gmail.com>
2016-07-07 18:49:35 +0100

    refining the map

--- js/maps.js
+++ js/maps.js
@@ -2,7 +2,7 @@ var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 51.5074, lng: 0.1278},
-    zoom: 12
+    zoom: 15
   });
   var infoWindow = new google.maps.InfoWindow({map: map});
 
